a1dc71da32c7d0b1fc62f5bdaac1981caa532295
est22 <anlia.seoul@gmail.com>
2024-10-04 15:34:52 +0900

    get users DB

--- api.js
+++ api.js
@@ -11,6 +11,15 @@ const db = new sqlite3.Database(db_name);
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  const users_sql = `select * from users`;
+  db.all(users_sql, [], (err, rows) => {
+    res.json({ users: rows });
+  });
+});
+
 app.listen(PORT, (req, res) => {
   console.log(`running server...`);
 });
